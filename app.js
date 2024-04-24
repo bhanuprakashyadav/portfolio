b620dde6434e51105b8f18a10188590ff9c3a110
Bhanu Prakash Yadav <bhanuprakashyadav909@gmail.com>
2024-04-24 23:14:27 +0530

    main page changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var i = 0;
 var j =1;
 var txt = 'WEB DEVELOPER AND PROGRAMMER'; /* The text */
-var speed = 300; /* The speed/duration of the effect in milliseconds */
-const profession = document.querySelector(".profession");
+var speed = 500; /* The speed/duration of the effect in milliseconds */
+const profession = document.querySelector(".move");
 
 setInterval(function typeWriter() {
     
